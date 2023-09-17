03f37a386ecf02d7de47e16c21b3e23d6271938e
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2023-09-16 21:48:21 -0400

    AED lab1

---
+++ Lab_1/src/ui/Project.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui;
+
+import model.Product;
+
+/**
+ *
+ * @author deepakkumar
+ */
+public class Project extends Product {
+
+    public Project() {
+    }
+    
+}
